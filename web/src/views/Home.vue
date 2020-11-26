e7ba7b06035c3b715c7c319344e766deb0cd08f1
masy <masy8511@fxiaoke.com>
2020-11-26 16:33:26 +0800

    走马灯

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home">
-    11122223333
+    <el-carousel indicator-position="outside" height="560px">
+      <el-carousel-item v-for="(item, index) in imgList" :key="index">
+        <img :src="require('../assets/img/banner/'+item)" alt="">
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      imgList: ['index_banner1.png','index_banner2.svg','index_banner1.png','index_banner2.svg']
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
 .home {
   height: 1000px;
   padding-top: 128px;
+}
+.el-carousel__item img {
+  width: 1920px;
+  height: 560px;
 }
 </style>
